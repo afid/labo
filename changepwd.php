d344d4f1ab92d7cb012f205914bc359b5c50d4ad
afid <afid.benayad@gmail.com>
2015-12-12 03:09:59 +0100

    version1.1

---
+++ changepwd.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: afidb
+ * Date: 11/12/2015
+ * Time: 23:55
+ */
+session_start();
+require_once("includes/config.php");
+
+$email = nl2br(htmlspecialchars(trim($_GET['email'])));
+$token = nl2br(htmlspecialchars(trim($_GET['sec'])));
+
+$pwd1 = trim($_POST['pwd1']);
+$pwd2 = trim($_POST['pwd2']);
+
+$title = "Changepwd";
+$mode = trim($_POST["mode"]);
+if ($mode == "change_password") {
+ // Si mot de passe non renseigné
+ if ($pwd1 == "" || $pwd2 == "") {
+  $_SESSION["errorType"] = "danger";
+  $_SESSION["errorMsg"] = "Veuillez renseigner tous les champs";
+  $_SESSION["login"] = null;
+ }
+ // Fin Si mot de passe non renseigné
+ // Si mot de passe different
+ elseif ($pwd1 != $pwd2 ) {
+  $_SESSION["errorType"] = "danger";
+  $_SESSION["errorMsg"] = "Les deux mots de passe ne sont pas identiques";
+ }
+ // Fin Si mot de passe different
+ // Si Formulaire corectement renseigné
+ else {
+  $sql = "SELECT * FROM labo_employee WHERE email = :email AND token = :token ";
+   try {
+    $stmt = $DB->prepare($sql);
+    // bind the values
+    $stmt->bindValue(":email", $email);
+    $stmt->bindValue(":token", $token);
+    // execute Query
+    $stmt->execute();
+    $results = $stmt->fetchAll();
+    // Si résultat dans Base de donnée
+    if (count($results) > 0) {
+     // Si compte actif
+     if ($results[0]["active"] == 1) {
+      // Si date token superieur a date actuelle Token encore Valide
+      if ($results[0]["token_expire"] > date('Y-m-d H:i:s')) {
+      // Changement de mot de passe
+       $sql = "UPDATE labo_employee SET password = :pwd1, token='', token_expire = '' WHERE email = :email and token = :token";
+       try {
+        $stmt = $DB->prepare($sql);
+        $stmt->bindValue(":pwd1", md5($pwd1));
+        $stmt->bindValue(":email", $email);
+        $stmt->bindValue(":token", $token);
+        $stmt->execute();
+        if ($stmt->rowCount()) {
+         $_SESSION["errorType"] = "success";
+         $_SESSION["errorMsg"] = '<i class="icon fa fa-check"></i>Votre mot a été modifié avec succès.';
+        } else {
+         $_SESSION["errorType"] = "warning";
+         $_SESSION["errorMsg"] = '<i class="icon fa fa-check"></i>Une erreur est survenue.';
+        }
+       } catch (Exception $e){
+       $_SESSION["errorType"] = "warning";
+       $_SESSION["errorMsg"] = '<i class="icon fa fa-info"></i>Une erreur est survenue.';
+      }
+      // Fin Changement de mot de passe
+      redirect("login.php");
+      exit;
+      }
+      // Fin Si date token Valide
+      // Si date token invalide
+      else{
+       $_SESSION["errorType"] = "warning";
+       $_SESSION["errorMsg"] = "<i class=\"icon fa fa-arrow-circle-o-down\"></i>Votre demande n'est plus valide";
+       redirect("forgotpwd.php");
+      }
+      // Fin Si date token invalide
+     // Fin Si compte actif
+     // Si compte suspendu
+     } else {
+      $_SESSION["errorType"] = "warning";
+      $_SESSION["errorMsg"] = "<i class=\"icon fa fa-info\"></i>Votre compte est suspendu.";
+      redirect("index.php");
+     }
+     // Fin Si compte suspendu
+    }
+    // Fin Si résultat dans Base de donnée
+    // Si tentative de Hack
+    else {
+     $_SESSION["errorType"] = "warning";
+     $_SESSION["errorMsg"] = "<i class=\"icon fa fa-arrow-circle-o-down\"></i>Votre demande ne peut aboutir";
+     redirect("forgotpwd.php");
+    }
+    // Fin Si tentative de Hack
+   }
+   // Si erreur SQL
+   catch (Exception $ex){
+    $_SESSION["errorType"] = "danger";
+    $_SESSION["errorMsg"] = $ex->getMessage();
+    redirect("index.php");
+   }
+   // Si erreur SQL
+ }
+ // Fin Si Formulaire corectement renseigné
+
+redirect("changepwd.php?email=" . $email . "&sec=".$token."");
+// recherche de la validité du mail et du token
+
+ // si oui
+  // recherche date de validité du token
+   // si oui Modification du mot de passe et suppression du token et date token
+    // Redirection page de login
+   // si date token non valide Message demande plus a jour
+   // Redirection page demande changement mot de passe
+ // si non Message demande non valide
+ // Redirection page demande changement mot de passe
+
+
+
+
+} // Fin de Validation du formulaire
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <?php
+  require_once 'includes/metas.php';
+  ?>
+<!-- iCheck -->
+<link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+</head>
+<body class="hold-transition login-page background_img">
+
+
+<div class="login-box">
+
+ <?php if ($ERROR_MSG <> "") { ?>
+  <!-- <div class="col-lg-12 login-box"> -->
+  <div class="alert no-marge alert-dismissable alert-<?php echo $ERROR_TYPE ?>">
+   <button data-dismiss="alert" class="close" type="button">x</button>
+   <p><?php echo $ERROR_MSG; ?></p>
+  </div>
+  <!-- </div> -->
+ <?php } ?>
+ <!-- /.login-logo -->
+ <div class="login-box-body">
+
+  <p class="login-box-msg">Changer votre mot de passe</p>
+  <p>Dans le premier champ saisisez votre nouveau mot de passe choisi, puis confirmez le dans le second.</p>
+
+  <form action="changepwd.php?email=<?php echo $email; ?>&sec=<?php echo $token; ?>" method="post" name="change_password" id="change_password">
+   <input type="hidden" name="mode" value="change_password">
+
+   <div class="form-group has-feedback">
+    <input type="password" class="form-control" placeholder="Mot de passe" id="pwd1" name="pwd1">
+    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+   </div>
+
+
+   <div class="form-group has-feedback">
+    <input type="password" class="form-control" placeholder="Confirmer votre Mot de passe" id="pwd2" name="pwd2">
+    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+   </div>
+   <div class="row">
+    <div class="col-xs-8">
+    </div>
+    <!-- /.col-xs-8 -->
+    <div class="col-xs-4">
+     <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+    </div>
+    <!-- /.col-xs-4 -->
+   </div>
+   <div class="form-group has-feedback fsPwdMessage">
+    <p>Le mot de passe doit être conforme à la politique de sécurité mise en place, à savoir que sa taille doit être d'au minimum 8 caractères, ne doit contenir le nom, prénom ou identifiant du compte utilisateur et doit contenir au moins 3 types de caractères différents dans les familles de caractères suivantes :
+     <ul>
+     <li>Chiffres : 0 1 2 3...</li>
+     <li>Lettres minuscules : a b c... z</li>
+     <li>Lettres majuscules : A B C... Z</li>
+     <li>Caractères spéciaux : $ % # @ ; , . ...</li>
+    </ul>
+    </p>
+   </div>
+   <!-- /.row -->
+  </form>
+ </div>
+ <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.1.4 -->
+<script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
--- forgotpwd.php
+++ forgotpwd.php
@@ -1,101 +1,91 @@
 <?php
-session_start();
-require_once("includes/config.php");
-// si la session existe
-if (isset($_SESSION["login"]) && $_SESSION["login"] != "") {
-  // if logged in send to dashboard page
-  redirect("index2.php");
-}
-
-$title = "Forgot password";
-$mode = trim($_POST["mode"]);
-if ($mode == "get_password") {
-  $email = trim($_POST['email']);
-
-if (isset($email) && $email == "") {
-    $_SESSION["errorType"] = "danger";
-    $_SESSION["errorMsg"] = '<i class="icon fa fa-ban"></i>Veuillez renseigner votre Email.';;
-  } else {
-    $sql = "SELECT * FROM labo_employee WHERE email = :email";
-    try {
-      $stmt = $DB->prepare($sql);
-
-      // bind the values
-      $stmt->bindValue(":email", $email);
-
-      // execute Query
-      $stmt->execute();
-      $results = $stmt->fetchAll();
-
-      if (count($results) > 0) {
-        if ($results[0]["active"] == 1) {
-          // envoie de mail
-          define('MAIL_SUJET','Votre identifiant');
-          $token = md5(uniqid(mt_rand()));
-          // Préparation Envoie du mail et update de la la table users
-          // Préparation de l'entête du mail:
-          $mail_entete  = 'From: "cmandgo.me"<'.ADMIN_MAIL.'>'."\r\n";
-          $mail_entete .= 'Reply-To: "cmandgo.me"<'.ADMIN_MAIL.'>'."\r\n";
-          $mail_entete .= 'Mime-Version: 1.0'."\r\n";
-          $mail_entete .= 'Content-type: text/html; charset=utf-8'."\r\n";
-          // Corp du mail
-          // préparation du corps du mail
-          $mail_corps  = "<b>Votre identification sur cmandgo.me</b><br />";
-          $mail_corps .= "Monsieur : ".$results[0]['firstname']." ".$results[0]['lastname']."<br />";
-          $mail_corps .="Pour finaliser le changement de votre mot de passe, nous vous invitons &agrave; cliquer sur le lien ci-dessous. Vous acc&eacute;derez directement &agrave; notre site afin de modifier votre mot de passe en toute simplicit&eacute; : <hr>";
-          $mail_corps .= "<a href=http://".$_SERVER['HTTP_HOST'].REPSITE."/passwordconfirm.php?email=$email&ConnID=$token>Modifier mon mot de passe</a><hr>";
-          $mail_corps .="En cas de dysfonctionnement, veuillez copier puis coller le lien <strong>http://".$_SERVER['HTTP_HOST']."/passwordconfirm.php?email=$email&ConnID=$token</strong> dans le champ \"adresse\" de votre navigateur Internet (Internet Explorer, Netscape, Mozilla...).<hr>";
-          $mail_corps .="Ce lien restera disponible 48h, &agrave; la suite de quoi vous devrez r&eacute;it&eacute;rer votre demande. <br />";
-          $mail_corps .="Si vous ne savez pas pourquoi vous avez re&ccedil;u cet e-mail, ignorez-le simplement ou bien contactez notre service Support.<br />";
-          $mail_corps .="Nous vous remercions d'avoir utilis&eacute; <a href='afid.noip.me/labo'>afid.noip.me/labo</a> et vous souhaitons une agr&eacute;able visite sur notre site.";
-          // fin corp du mail
-          if (mail($email,MAIL_SUJET,$mail_corps,$mail_entete)) {
-          // Mise a jour du token
-            //$sql = "UPDATE labo_employee SET token='token' WHERE email = $email";
-            $sql = "UPDATE labo_employee SET token = :token, token_expire = now()+ INTERVAL 2 DAY WHERE email = :email";
-            try {
-              $stmt = $DB->prepare($sql);
-              $stmt->bindValue(":token", $token);
-              $stmt->bindValue(":email", $email);
-              $stmt->execute();
-              if ($stmt->rowCount()) {
-                $_SESSION["errorType"] = "success";
-                $_SESSION["errorMsg"] = '<i class="icon fa fa-check"></i>Veuillez verifier votre boite mail.';
-              } else {
-                $_SESSION["errorType"] = "warning";
-                $_SESSION["errorMsg"] = '<i class="icon fa fa-check"></i>Une erreur est survenue.';
-              }
-            } catch (Exception $e){
-              $_SESSION["errorType"] = "warning";
-              $_SESSION["errorMsg"] = '<i class="icon fa fa-info"></i>Une erreur est survenue.';
-
-            }
-          // Fin Mise a jour du token
-          }
-          // fin envoi de mail
-          redirect("forgotpwd.php");
-          exit;
-        } else {
-          $_SESSION["errorType"] = "warning";
-          $_SESSION["errorMsg"] = '<i class="icon fa fa-info"></i>Votre compte n\'est pas activ&eacute;.';
-        }
-      } else {
-        $_SESSION["errorType"] = "warning";
-        $_SESSION["errorMsg"] = '<i class="icon fa fa-ban"></i>OUPS... Votre email est inexistant';
-      }
-    } catch (Exception $ex) {
-      $_SESSION["errorType"] = "danger";
-      $_SESSION["errorMsg"] = $ex->getMessage();
-    }
+  session_start();
+  require_once("includes/config.php");
+  // si la session existe
+  if (isset($_SESSION["login"]) && $_SESSION["login"] != "") {
+    // if logged in send to dashboard page
+    redirect("index2.php");
   }
-  redirect($_SERVER['PHP_SELF']);
-} // end if login
+
+  $title = "Forgot password";
+  $mode = trim($_POST["mode"]);
+  if ($mode == "get_password") {
+    $email = trim($_POST['email']);
+    if (isset($email) && $email == "") {
+      $_SESSION["errorType"] = "danger";
+      $_SESSION["errorMsg"] = '<i class="icon fa fa-ban"></i>Veuillez renseigner votre Email.';;
+    } else {
+      $sql = "SELECT * FROM labo_employee WHERE email = :email";
+      try {
+        $stmt = $DB->prepare($sql);
+        // bind the values
+        $stmt->bindValue(":email", $email);
+        // execute Query
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+
+        if (count($results) > 0) {
+          if ($results[0]["active"] == 1) {
+            // envoie de mail
+            define('MAIL_SUJET','Votre identifiant');
+            $token = md5(uniqid(mt_rand()));
+            // Préparation Envoie du mail et update de la la table users
+            // Préparation de l'entête du mail:
+            $mail_entete  = 'From: "cmandgo.me"<'.ADMIN_MAIL.'>'."\r\n";
+            $mail_entete .= 'Reply-To: "cmandgo.me"<'.ADMIN_MAIL.'>'."\r\n";
+            $mail_entete .= 'Mime-Version: 1.0'."\r\n";
+            $mail_entete .= 'Content-type: text/html; charset=utf-8'."\r\n";
+            // Corp du mail
+            // préparation du corps du mail
+            $mail_corps = '<body topmargin="0" rightmargin="0" bottommargin="0" leftmargin="0" marginwidth="0" marginheight="0" width="100%"><div bgcolor="#e0e2e4"><div dir="" style="padding:0px;"><table align="center" bgcolor="#e0e2e4" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><table style="margin:0 auto" align="center" bgcolor="#e0e2e4" cellpadding="0" cellspacing="0" width="600"><tbody><tr><td><table style="border:0" align="center" border="0" cellpadding="0" cellspacing="0" width="600"><tbody><tr><td style="padding:0px">&nbsp;</td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table><table align="center" bgcolor="#e0e2e4" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><table style="margin:0 auto" align="center" bgcolor="#e0e2e4" cellpadding="0" cellspacing="0" width="600"><tbody><tr><td style="border:1px solid #d9d9da;border-bottom:0px" bgcolor="#fcfefc"><table style="width:100%;margin:0 auto!important" align="center" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><table style="margin:0 auto!important;padding:15px 0" align="center" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td style="padding:0 0 0 10px" height="40"><h1 style="font-family:Arial;font-size:22px;font-weight:normal;letter-spacing:-1px;margin:0!important">R&eacute;nitialisez votre mot de passe</h1></td></tr></tbody></table></td></tr></tbody></table><table style="padding:2px 0;width:100%;margin:0 auto!important" align="center" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><table style="margin:0 auto!important" align="center" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td style="text-align:center;text-align:center" height="26"><span style="margin:0 auto;color:#9e9ea2;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;font-size:12px">Bonjour '.$results[0]["firstname"].' '.$results[0]["lastname"].', vous avez r&eacute;cemment demand&eacute; un nouveau mot de passe.</span></td></tr><tr><td style="text-align:center;text-align:center"><span style="margin:0 auto;color:#9e9ea2;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;font-size:12px">Cliquez sur le bouton "R&eacute;initialiser mon mot de passe".</span></td></tr></tbody></table></td></tr></tbody></table><table style="width:100%;margin:0 auto!important;padding:15px 0" align="center" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><table style="margin:0 auto!important" align="center" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td style="padding:10px 15px;font-weight:bold;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;font-size:12px;text-align:center" bgcolor="#f07355"><a href="http://'.$_SERVER["HTTP_HOST"].'/labo/changepwd.php?email='.$email.'&sec='.$token.'" style="text-decoration:none;color:#ffffff" title="R&eacute;initialiser mon mot de passe" target="_blank">R&eacute;initialiser mon mot de passe</a></td></tr></tbody></table></td></tr></tbody></table></td></tr><tr><td style="border:1px solid #d9d9da;border-bottom:0px;border-top:0px;padding:15px 0" align="center" bgcolor="#f6f7f8"><table style="margin:0 auto" align="center" cellpadding="0" cellspacing="0"><tbody><tr><td style="text-align:center"><span style="color:#9f9f9f;font-size:11px;margin:0 auto;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;padding:0"> Ce lien de r&eacute;initialisation est valable 48h. Au del&agrave;, vous devrez faire une <a href="http://'.$_SERVER["HTTP_HOST"].'/labo/forgotpwd.php" title="Faire une nouvelle demande" style="color:#42a2d6;font-weight:bold;text-decoration:none" target="_blank">nouvelle demande</a>. </span></td></tr><tr><td style="text-align:center"><span style="color:#9f9f9f;font-size:11px;margin:0 auto;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;padding:0"> Vous n\'&ecirc;tes pas &agrave; l\'origine de cette demande ? <a href="mailto:afid.benayad@gmail.com" title="Nous contacter" style="color:#42a2d6;font-weight:bold;text-decoration:none" target="_blank">Contactez-nous</a>. </span></td></tr></tbody></table></td></tr></tbody></table><table style="margin:0!important;padding:0!important" align="center" bgcolor="#e0e2e4" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><table style="margin:0 auto!important;padding:0!important" align="center" bgcolor="#e0e2e4" cellpadding="0" cellspacing="0"><tbody><tr style="margin:0!important;padding:0!important"><td style="margin:0!important;padding:0!important"><img src="http://afid.noip.me/labo/dist/img/DCR.jpg" alt="&nbsp;" style="display:block;margin:0 auto!important;padding:0!important" border="0"></td></tr><tr><td><table style="margin:0 auto" align="center" cellpadding="0" cellspacing="0"><tbody><tr><td style="padding:5px 0 0 0!important"><span style="color:#9f9f9f;font-size:9px;margin:0 auto;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;width:100%;text-align:center;display:block">Ce message est destin&eacute; &agrave; Afid Benayad</span></td></tr></tbody></table></td></tr><tr><td><table style="margin:0 auto" align="center" cellpadding="0" cellspacing="0"><tbody><tr><td><span style="color:#9f9f9f;font-size:9px;margin:0 auto;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;width:100%;text-align:center;display:block"> Vous rencontrez des difficult&eacute;s ? Consultez notre <a href="http://'.$_SERVER["HTTP_HOST"].'/howto.html" title="Aide" style="color:#545357;text-decoration:none" target="_blank">aide en ligne</a>. </span></td></tr><tr><td><span style="color:#9f9f9f;font-size:9px;margin:0 auto;font-family:Arial,\'Helvetica Neue\',Helvetica,sans-serif;width:100%;text-align:center;display:block">A tr&egrave;s bient&ocirc;t, </span></td></tr><tr><td>&nbsp;</td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></div></div></body>';
+            // fin corp du mail
+            if (mail($email,MAIL_SUJET,$mail_corps,$mail_entete)) {
+              // Mise a jour du token
+              $sql = "UPDATE labo_employee SET token = :token, token_expire = now()+ INTERVAL 2 DAY WHERE email = :email";
+              try {
+                $stmt = $DB->prepare($sql);
+                $stmt->bindValue(":token", $token);
+                $stmt->bindValue(":email", $email);
+                $stmt->execute();
+                if ($stmt->rowCount()) {
+                  $_SESSION["errorType"] = "success";
+                  $_SESSION["errorMsg"] = '<i class="icon fa fa-check"></i>Veuillez verifier votre boite mail.';
+                } else {
+                  $_SESSION["errorType"] = "warning";
+                  $_SESSION["errorMsg"] = '<i class="icon fa fa-check"></i>Une erreur est survenue.';
+                }
+              } catch (Exception $e){
+                $_SESSION["errorType"] = "warning";
+                $_SESSION["errorMsg"] = '<i class="icon fa fa-info"></i>Une erreur est survenue.';
+              }
+              // Fin Mise a jour du token
+              redirect("login.php");
+              exit;
+            }
+            // fin envoi de mail
+            // Si le compte n'est pas actif
+          } else {
+            $_SESSION["errorType"] = "warning";
+            $_SESSION["errorMsg"] = '<i class="icon fa fa-info"></i>Votre compte n\'est pas activ&eacute;.';
+          }
+        } else {
+          // Si email introuvable en DB
+          $_SESSION["errorType"] = "warning";
+          $_SESSION["errorMsg"] = '<i class="icon fa fa-ban"></i>OUPS... Votre email est inexistant';
+        }
+      } catch (Exception $ex) {
+        $_SESSION["errorType"] = "danger";
+        $_SESSION["errorMsg"] = $ex->getMessage();
+      }
+    }
+    redirect($_SERVER['PHP_SELF']);
+  } // end if login
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <?php
-  require_once 'includes/metas.php';
+    require_once 'includes/metas.php';
   ?>
   <!-- iCheck -->
   <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
@@ -115,7 +105,7 @@ if (isset($email) && $email == "") {
   <?php } ?>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Perte de mot de passe</p>
+    <h3>Réinitialiser votre mot de passe</h3>
 
     <form action="#" method="post" name="contact_form" id="contact_form">
       <input type="hidden" name="mode" value="get_password">
@@ -125,8 +115,8 @@ if (isset($email) && $email == "") {
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <p class="text-aqua text_left">Indiquez votre email puis cliquez sur "Demande de mot de passe".</p>
-        <p> Vous recevrez un email qui vous expliquera les &eacute;tapes &agrave; suivre.</p>
+        <p class="text-aqua text_left">Après avoir saisi votre email, consultez votre messagerie : vous y trouverez un mail vous expliquant comment créer un nouveau mot de passe.</p>
+				<p>En effet, pour des raisons de confidentialité, nous ne renvoyons pas les mots de passe par email.</p>
       </div>
       <div class="row">
         <div class="col-xs-4">
